3bd963b8c11cbcbba05a6a7d209cb63b3143e944
CXPhoenix <jate9229@gmail.com>
2022-06-03 00:53:21 +0800

    feat: 🎸 v2: change to dark mode

--- src/main.js
+++ src/main.js
@@ -5,5 +5,28 @@ form.addEventListener("submit", (e) => {
   e.preventDefault();
 });
 
+const p = document.querySelector("#email-text");
+form["googleAddr"].addEventListener("input", (e) => {
+  if (e.target.value === "") {
+    p.classList.remove(
+      "left-[0.4rem]",
+      "-translate-y-[1.45rem]",
+      "bg-[#222]",
+      "px-[6px]",
+      "text-xs",
+      "text-blue-700"
+    );
+    return;
+  }
+  p.classList.add(
+    "left-[0.4rem]",
+    "-translate-y-[1.45rem]",
+    "bg-[#222]",
+    "px-[6px]",
+    "text-xs",
+    "text-blue-700"
+  );
+});
+
 const red = "#ea4335";
 const yellow = "#fbbc05";
